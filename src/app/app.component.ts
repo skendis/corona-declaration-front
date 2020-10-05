5c84f67cb7bc452f3a0528e5661012e790cbbbfe
skendis <sergei.kendis@gmail.com>
2020-10-05 23:29:09 +0300

    add i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'corona-declaration-front';
+
+  constructor(public translate: TranslateService, @Inject(DOCUMENT) private document: Document) {
+    translate.addLangs(['he', 'en']);
+    this.changeLanguage('he');
+  }
+
+  /**
+   * @description change default site language
+   */
+  changeLanguage(lang: string): void {
+    const htmlTag = this.document.getElementsByTagName('html')[0] as HTMLHtmlElement;
+    htmlTag.dir = lang === 'he' ? 'rtl' : 'ltr';
+    htmlTag.lang = lang;
+    this.translate.setDefaultLang(lang);
+    this.translate.use(lang);
+  }
 }
